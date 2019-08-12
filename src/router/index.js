88622acd1214f65df84c32dd1aefbb47f3e9af58
王广开 <416266475@qq.com>
2019-08-12 17:25:04 +0800

    ...

--- src/router/index.js
+++ src/router/index.js
@@ -27,7 +27,7 @@ router.beforeEach((to, from, next) => {
   } else {
     store.commit('delAliveExclude', 'SuggestionList');
   }
-  const timestamp = new Date().getTime();
+  const timestamp = `${new Date().getTime()}`.substr(0, 10);
   axPost(
     '/test/vipWeChat/jsSdk/getJsSign',
     {
@@ -36,15 +36,14 @@ router.beforeEach((to, from, next) => {
       timestamp,
       url: window.location.href
     }
-  ).then(response => {
-    debugger
+  ).then((response) => {
     wx.config({
-      debug: true,
-      appId: 'wx2409ae17ef4a9f34',
-      timestamp: timestamp,
-      nonceStr: timestamp,
-      signature: response.data.jsSignature,
-      jsApiList: ['scanQRCode']
+      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: 'wx2409ae17ef4a9f34', // "wx1514378cc449f87d", // 必填，公众号的唯一标识
+      timestamp, // 必填，生成签名的时间戳
+      nonceStr: timestamp, // 必填，生成签名的随机串
+      signature: response.data.jsSignature, // 必填，签名，见附录1
+      jsApiList: ['scanQRCode'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
   });
   next();
--- src/views/sales/SalesVerification.vue
+++ src/views/sales/SalesVerification.vue
@@ -375,7 +375,7 @@ export default {
         });
       });
     },
-    searchByCondition(){
+    searchByCondition() {
       this[this.curScrollViewName].mescroll.triggerDownScroll();
     },
     search(page) {
@@ -458,7 +458,7 @@ export default {
             orderName: v.hmcName,
             errorReason: v.ehubMsg
           }));
-          debugger
+          debugger;
         }
       });
       if (isShowDetail) {
@@ -519,12 +519,12 @@ export default {
       });
     },
     salesScanQRCode() {
-      wx.ready(function () {
+      wx.ready(() => {
         wx.scanQRCode({
           needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-          scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-          success: function (res) {
-            var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+          scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
+          success(res) {
+            const result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
           }
         });
       });
