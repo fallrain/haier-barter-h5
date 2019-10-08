426f5a5ad00049b7fa4378cb45ff4779f01056b9
王广开 <416266475@qq.com>
2019-10-08 11:23:17 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -31,11 +31,10 @@ router.beforeEach((to, from, next) => {
   next();
 });
 router.afterEach(() => {
-  alert(window.location.href)
   Vue.prototype.basicService.jsSign(window.location.href).then(({ code, data }) => {
     if (code === 1) {
       wx.config({
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         ...data,
         jsApiList: [
           'scanQRCode',
