85dfcb0a95e63626f27ead77481fd7c9ac6163c0
王广开 <416266475@qq.com>
2019-11-03 13:04:10 +0800

    权益样式修改

--- src/components/form/BActivityItem.vue
+++ src/components/form/BActivityItem.vue
@@ -1,124 +1,165 @@
 <template>
-  <div class="activity-item-bg" >
+  <div class="activity-item-bg">
     <!--<div v-show="!hasData">-->
-      <!--<p>暂无权益活动可选</p>-->
+    <!--<p>暂无权益活动可选</p>-->
     <!--</div>-->
-    <div >
-    <div class="activity-gift">
-      <i class="iconfont icon-liwu activity-gift-img"/>
-      <span class="activity-gift-title">{{getData.rightsName}}</span>
-    </div>
-    <div class="activity-common-line activity-common-text activity-reason-text" v-if="getData.reason && isFinish">
-      不可参与原因：{{getData.reason}}
-    </div>
-    <div class="activity-common-line activity-common-text">活动品牌：{{getData.rightsBrandC}}</div>
-    <div class="activity-common-line activity-common-text">试用范围：{{getData.rightsProductCategory}}</div>
-    <div class="activity-common-line activity-common-text">
-      活动日期：<span class="activity-data-text">{{getData.activityStartDate}}至{{getData.activityEndDate}}</span>
-    </div>
-      <div class="activity-common-line activity-item activity-common-top activity-common-border" @click="setShowConfig">
+    <div>
+      <div class="activity-gift">
+        <i class="iconfont icon-liwu activity-gift-img"/>
+        <span class="activity-gift-title">{{getData.rightsName}}</span>
+      </div>
+      <div class="activity-common-line activity-common-text activity-reason-text" v-if="getData.reason && isFinish">
+        不可参与原因：{{getData.reason}}
+      </div>
+      <div class="activity-common-line activity-common-text">活动品牌：{{getData.rightsBrandC}}</div>
+      <div class="activity-common-line activity-common-text">试用范围：{{getData.rightsProductCategory}}</div>
+      <div class="activity-common-line activity-common-text">
+        活动日期：<span class="activity-data-text">{{getData.activityStartDate}}至{{getData.activityEndDate}}</span>
+      </div>
+      <div class="activity-common-line activity-titleItem activity-common-top activity-common-border" @click="setShowConfig">
         <span class="activity-tip-text activity-count-remain">查看礼品</span>
         <span class="icon iconfont icon-jiantou9 activity-tip-img activity-count-remain"
               :class="{'activity-tip-img-transform': isShowConfig}"></span>
       </div>
-    <div v-for="(item,index) in getData.configList"
-         :key="index" v-show="isShowConfig">
-      <!--单品-->
-      <div class="activity-common-line activity-common-border activity-common-top activity-item" v-show="getData.rightsType == 'single'">
-        <div class="activity-item"  v-show=" getData.singleProductMode == 'single'">
-          <span class="activity-name-icon">购</span>
-          <span class="activity-common-text activity-common-left activity-common-product">{{item.productModel}}{{item.industryName}}</span>
+      <div v-for="(item,index) in getData.configList"
+           :key="index" v-show="isShowConfig">
+        <!--单品-->
+        <div class="activity-common-line activity-common-border activity-common-top activity-titleItem"
+             v-show="getData.rightsType == 'single'">
+          <div class="activity-item" v-show=" getData.singleProductMode == 'single'">
+            <span class="activity-name-icon">购</span>
+            <span class="activity-common-left activity-common-product">{{item.productModel}}{{item.industryName}}</span>
+          </div>
+          <div class="activity-item" v-show=" getData.singleProductMode == 'multi' ">
+            <span class="activity-name-icon">购</span>
+            <span class="activity-common-left activity-common-product" v-for="(pro,i) in item.rightsProductDtoList">{{pro.productModel}}{{pro.industryName}}、</span>
+          </div>
+          <div class="activity-item" v-show=" getData.singleProductMode == 'all' ">
+            <span class="activity-name-icon">购</span>
+            <span class="activity-common-left activity-common-product">全部型号</span>
+          </div>
+          <span class="activity-common-product activity-common-left activity-float">共<span
+            class="activity-type-text">{{item.rightsGiftTotal}}</span>份，剩余<span class="activity-count-remain">{{item.rightsGiftSurplus}}</span>份</span>
         </div>
-        <div class="activity-item"  v-show=" getData.singleProductMode == 'multi' ">
-          <span class="activity-name-icon">购</span>
-          <span class="activity-common-text activity-common-left activity-common-product" v-for="(pro,i) in item.rightsProductDtoList">{{pro.productModel}}{{pro.industryName}}、</span>
-        </div>
-        <div class="activity-item"  v-show=" getData.singleProductMode == 'all' ">
-          <span class="activity-name-icon">购</span>
-          <span class="activity-common-text activity-common-left activity-common-product">全部型号</span>
-        </div>
-        <span class="activity-common-text activity-common-left activity-float">共<span
-          class="activity-type-text">{{item.rightsGiftTotal}}</span>份，剩余<span class="activity-count-remain">{{item.rightsGiftSurplus}}</span>份</span>
-      </div>
 
-      <!--套购-->
-      <div class="activity-common-line activity-common-border activity-common-top activity-item" v-show="getData.rightsType == 'sets'">
-       <!--按特定型号组合-->
-        <div class="activity-item"  v-show="getData.setsCombineMode == '1'">
-          <span class="activity-name-icon">购</span>
-          <span class="activity-product" v-for="(rightsSetsCombine ,j) in item.rightsSetsCombineDtoList">
+        <!--套购-->
+        <div class="activity-common-line activity-common-border activity-common-top activity-titleItem"
+             v-show="getData.rightsType == 'sets'">
+          <!--按特定型号组合-->
+          <div class="activity-item" v-show="getData.setsCombineMode == '1'">
+            <span class="activity-name-icon">购</span>
+            <span class="activity-product" v-for="(rightsSetsCombine ,j) in item.rightsSetsCombineDtoList">
             <span v-show="rightsSetsCombine.productModel == null">所有{{rightsSetsCombine.industryName}}
             </span>
-            <span v-show="item.productCombineSymbols == 'and' && j !== item.rightsSetsCombineDtoList.length - 1">+</span>
+            <span
+              v-show="item.productCombineSymbols == 'and' && j !== item.rightsSetsCombineDtoList.length - 1">+</span>
           <span v-show="item.productCombineSymbols == 'or' && j !== item.rightsSetsCombineDtoList.length - 1">/</span>
           </span>
-          <span class="activity-product" v-for="(rightsSetsCombine ,j) in item.rightsSetsCombineDtoList">
+            <span class="activity-product" v-for="(rightsSetsCombine ,j) in item.rightsSetsCombineDtoList">
             <span v-show="rightsSetsCombine.productModel != null">
               <span v-for="(rightsSetsPro ,l) in rightsSetsCombine.rightsProductDtoList">
                 {{rightsSetsPro.productModel}}{{rightsSetsPro.industryName}}
               </span>
-              <span v-show="item.productCombineSymbols == 'and' && j !== item.rightsSetsCombineDtoList.length - 1">+</span>
+              <span
+                v-show="item.productCombineSymbols == 'and' && j !== item.rightsSetsCombineDtoList.length - 1">+</span>
             <span v-show="item.productCombineSymbols == 'or' && j !== item.rightsSetsCombineDtoList.length - 1">/</span>
             </span>
           </span>
-          <!--<span v-show="item.productCombineSymbols == 'and'">+</span>-->
-          <!--<span v-show="item.productCombineSymbols == 'or'">/</span>-->
+            <!--<span v-show="item.productCombineSymbols == 'and'">+</span>-->
+            <!--<span v-show="item.productCombineSymbols == 'or'">/</span>-->
+          </div>
+          <!--按满赠-->
+          <div class="activity-item" v-show=" getData.setsCombineMode == '2'">
+            <span class="activity-name-icon">满</span>
+            <span class=" activity-common-left activity-span">{{item.priceRangeStart}}</span>
+            <span class=" activity-common-left activity-span">-</span>
+            <span class=" activity-common-left activity-span">{{item.priceRangeEnd}}</span>
+          </div>
+          <span class="activity-common-product activity-common-left activity-float">共<span
+            class="activity-type-text">{{item.rightsGiftTotal}}</span>份，剩余<span class="activity-count-remain">{{item.rightsGiftSurplus}}</span>份</span>
         </div>
-        <!--按满赠-->
-        <div class="activity-item"  v-show=" getData.setsCombineMode == '2'">
-          <span class="activity-name-icon">满</span>
-          <span class=" activity-common-left activity-span">{{item.priceRangeStart}}</span>
-          <span class=" activity-common-left activity-span">-</span>
-          <span class=" activity-common-left activity-span">{{item.priceRangeEnd}}</span>
+        <div class="activity-common-line activity-item activity-item-gift">
+          <span class="activity-name-icon">送</span>
+          <span class="activity-common-product activity-common-left activity-item-gift-desc"
+                v-show="getData.giveGiftMode == 'designated'">{{item.giftName}}</span>
+          <span class="activity-common-product activity-common-left activity-item-gift-desc"
+                v-show="getData.giveGiftMode == 'fixed'">{{item.giveIntegralValue}}</span>
+          <span class="activity-common-product activity-common-left activity-item-gift-desc"
+                v-show="getData.giveGiftMode == 'ratio'">{{item.integralPriceRatio}}</span>
+          <span class="activity-common-product activity-common-left activity-item-gift-desc"
+                v-show="getData.giftType == 'virtual'">{{item.virtualGiftName}}</span>
         </div>
-        <span class="activity-common-text activity-common-left activity-float">共<span
-          class="activity-type-text">{{item.rightsGiftTotal}}</span>份，剩余<span class="activity-count-remain">{{item.rightsGiftSurplus}}</span>份</span>
       </div>
-      <div class="activity-common-line activity-item activity-item-gift">
-        <span class="activity-name-icon">送</span>
-        <span class="activity-common-text activity-common-left activity-item-gift-desc" v-show="getData.giveGiftMode == 'designated'">{{item.giftName}}</span>
-        <span class="activity-common-text activity-common-left activity-item-gift-desc" v-show="getData.giveGiftMode == 'fixed'">{{item.giveIntegralValue}}</span>
-        <span class="activity-common-text activity-common-left activity-item-gift-desc" v-show="getData.giveGiftMode == 'ratio'">{{item.integralPriceRatio}}</span>
-        <span class="activity-common-text activity-common-left activity-item-gift-desc" v-show="getData.giftType == 'virtual'">{{item.virtualGiftName}}</span>
-      </div>
-    </div>
-    <div class="activity-common-line activity-item activity-common-top activity-common-border" @click="setShowLimit">
-      <span class="activity-tip-text activity-count-remain">其他限制</span>
-      <span class="icon iconfont icon-jiantou9 activity-tip-img activity-count-remain"
-            :class="{'activity-tip-img-transform': isShowLimit}"></span>
-    </div>
-
-    <div v-if="isShowLimit">
-      <div class="activity-common-line activity-common-text">
-        <p>用户可兑换日期：{{getData.exchangeStartTime}}至{{getData.exchangeEndTime}}</p>
-      </div>
-      <div class="activity-common-line activity-common-text">
-        <p v-show="getData.giftType =='virtual'">礼品类型：尊享卡</p>
-        <p v-show="getData.giftType =='jfmall'">礼品类型：海贝积分</p>
-        <p v-show="getData.giftType =='casarte'">礼品类型：卡萨帝积分</p>
-        <p v-show="getData.giftType =='entity'">礼品类型：实物礼品</p>
+      <div class="activity-common-line activity-titleItem activity-common-top activity-common-border" @click="setShowLimit">
+        <span class="activity-tip-text activity-count-remain">其他限制</span>
+        <span class="icon iconfont icon-jiantou9 activity-tip-img activity-count-remain"
+              :class="{'activity-tip-img-transform': isShowLimit}"></span>
       </div>
 
-      <div class="activity-common-line activity-common-text" v-for="(model,index) in getData.limitList.model">
-        <span>{{model.productBrandName}}：{{model.productGroupName}}<span v-show="model.productModel == '*'">全部型号</span><span v-show="model.productModel != '*'">{{model.productModel}}</span></span>排除在权益计算之外
-      </div>
-      <div class="activity-common-line activity-common-text" v-for="(price,index) in getData.limitList.price">
-        <p>{{price.productBrandName}}：{{price.productGroupName}}价格在{{price.limitPriceMin}}-{{price.limitPriceMax}}之间可参与套餐</p>
-      </div>
-      <div class="activity-common-line activity-common-text" v-for="(appoint,index) in getData.limitList.appoint">
-        <span>{{appoint.productBrandName}}：{{appoint.productGroupName}}<span v-show="appoint.productModel == '*'">全部型号</span><span v-show="appoint.productModel != '*'">{{appoint.productModel}}</span></span>必须包含其中任意一个
-      </div>
+      <div v-if="isShowLimit">
+        <div class="activity-common-line activity-common-text">
+          <p>用户可兑换日期：{{getData.exchangeStartTime}}至{{getData.exchangeEndTime}}</p>
+        </div>
+        <div class="activity-common-line activity-common-text">
+          <p v-show="getData.giftType ==='virtual'">礼品类型：尊享卡</p>
+          <p v-show="getData.giftType ==='jfmall'">礼品类型：海贝积分</p>
+          <p v-show="getData.giftType ==='casarte'">礼品类型：卡萨帝积分</p>
+          <p v-show="getData.giftType ==='entity'">礼品类型：实物礼品</p>
+        </div>
 
-    </div>
-    <img class="activity-img" v-if="isFinish && !hasData" src="../../assets/images/orderFollow-up/activity-img.png"/>
-    <div class="activity-number" v-if="!isFinish && !residueGift">
-      <span class="activity-number-minus activity-number-common" @click="minusCount" v-show="!getData.minesGray">-</span>
-      <span class="activity-number-minus activity-number-common-no" v-show="getData.minesGray">-</span>
+        <div
+          class="activity-common-line"
+          v-for="(model,index) in getData.limitList.model"
+          :key="index"
+        >
+          <span>
+            {{model.productBrandName}}：{{model.productGroupName}}
+            <span
+              v-show="model.productModel === '*'"
+            >全部型号</span>
+            <span
+              v-show="model.productModel !== '*'"
+            >{{model.productModel}}
+            </span>
+          </span>排除在权益计算之外
+        </div>
+        <div
+          class="activity-common-line"
+          v-for="(price,index) in getData.limitList.price"
+          :key="index"
+        >
+          <p>
+            {{price.productBrandName}}：{{price.productGroupName}}价格在{{price.limitPriceMin}}-{{price.limitPriceMax}}之间可参与套餐</p>
+        </div>
+        <div
+          class="activity-common-line"
+          v-for="(appoint,index) in getData.limitList.appoint"
+          :key="index"
+        >
+          <span>
+            {{appoint.productBrandName}}：{{appoint.productGroupName}}
+            <span
+              v-show="appoint.productModel === '*'"
+            >全部型号</span>
+            <span
+              v-show="appoint.productModel !== '*'"
+            >{{appoint.productModel}}
+            </span>
+          </span>
+          必须包含其中任意一个
+        </div>
 
-      <span class="activity-number-count">{{getData.selectedNum}}</span>
-      <span class="activity-number-add activity-number-common" @click="addCount" v-show="!getData.addGray">+</span>
-      <span class="activity-number-add activity-number-common-no" v-show="getData.addGray">+</span>
-    </div>
+      </div>
+      <img class="activity-img" v-if="isFinish && !hasData" src="../../assets/images/orderFollow-up/activity-img.png"/>
+      <div class="activity-number" v-if="!isFinish && !residueGift">
+        <span class="activity-number-minus activity-number-common" @click="minusCount"
+              v-show="!getData.minesGray">-</span>
+        <span class="activity-number-minus activity-number-common-no" v-show="getData.minesGray">-</span>
+
+        <span class="activity-number-count">{{getData.selectedNum}}</span>
+        <span class="activity-number-add activity-number-common" @click="addCount" v-show="!getData.addGray">+</span>
+        <span class="activity-number-add activity-number-common-no" v-show="getData.addGray">+</span>
+      </div>
     </div>
   </div>
 
@@ -157,7 +198,7 @@ export default {
       }
     },
     isFinish: false,
-    residueGift:false,
+    residueGift: false,
     hasData: false,
     rightsType: '',
 
@@ -194,7 +235,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
   .activity-item-bg {
     position: relative;
     margin-top: 16px;
@@ -225,22 +266,22 @@ export default {
     color: #333;
     margin-left: 15px;
     display: block;
-    word-break:normal;
-    width:400px;
-    white-space:pre-wrap;
-    word-wrap : break-word ;
-    overflow: hidden ;
+    word-break: normal;
+    width: 400px;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    overflow: hidden;
   }
 
   .activity-common-text {
-    word-break:break-all;
+    word-break: break-all;
     width: 600px;
     font-size: 24px;
     color: #666;
   }
 
   .activity-common-left {
-    margin-left: 20px;
+    // margin-left: 20px;
   }
 
   .activity-common-top {
@@ -282,6 +323,7 @@ export default {
     background: #fff;
     color: #F5A623;
     font-size: 24px;
+    margin-right: 16px;
     padding-top: 2px;
     padding-right: 5px;
     padding-bottom: 2px;
@@ -341,6 +383,7 @@ export default {
     height: 48px;
     text-align: center;
   }
+
   .activity-number-common-no {
     width: 48px;
     height: 48px;
@@ -353,14 +396,21 @@ export default {
   }
 
   .activity-common-product {
-    /*width: 366px;*/
-    width: 100px;
+    font-size: 24px;
+    color: #666;
   }
 
   .activity-item {
     display: flex;
-    justify-content: space-between;
+    flex-wrap: wrap;
     position: relative;
+  }
+
+  .activity-titleItem {
+    display: flex;
+    flex-wrap: wrap;
+    position: relative;
+    justify-content: space-between;
   }
 
   .activity-item-gift {
@@ -371,13 +421,14 @@ export default {
     flex-grow: 1;
     word-break: break-all;
   }
-  .activity-span{
+
+  .activity-span {
     font-size: 24px;
     color: #666;
   }
-  .activity-float{
-    position: absolute;
-    margin-left: 440px;
+
+  .activity-float {
+    margin-left: auto;
   }
 
   .activity-check-gift {
@@ -407,7 +458,8 @@ export default {
     font-size: 24px;
     color: #999;
   }
-  .activity-product{
+
+  .activity-product {
     font-size: 24px;
     color: #666;
   }
