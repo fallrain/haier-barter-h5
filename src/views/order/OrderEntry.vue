2878d8616e40b28de162c52aa4ac3dc973a5f8c5
fhj <haijiao_fan_yt@163.com>
2019-11-10 18:17:06 +0800

    修改测试bug

--- src/views/order/OrderEntry.vue
+++ src/views/order/OrderEntry.vue
@@ -882,7 +882,7 @@ export default {
         //     }
         //   });
         // }
-      } else {
+      // } else {
         this.generateSubInfo(1);
       }
     },
@@ -1195,7 +1195,7 @@ export default {
     // consporConfirm() {
     //   // this.saveTemporary(1);
     // },
-    next() {
+    next() {debugger
       /* 下一步 */
       this.saveType = 0;
       this.saveTemporary(2);
