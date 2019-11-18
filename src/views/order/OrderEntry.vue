53dd837583cbddb02cbc6de62b42abc49ebcd9b8
铁柱， <jym@jiyoumi.com>
2019-11-18 23:41:12 +0800

    bug fix

--- src/views/order/OrderEntry.vue
+++ src/views/order/OrderEntry.vue
@@ -627,8 +627,10 @@ export default {
         if (res.code === 1) {
           if (res.data.length !== 0) {
             this.rightsList = res.data;
-            ri.startTime = ri.startTime.substring(0, 10);
-            ri.endTime = ri.endTime.substring(0, 10);
+            this.rightsList.forEach(ri => {
+              ri.startTime = ri.startTime.substring(0, 10);
+              ri.endTime = ri.endTime.substring(0, 10);
+            })
           }
         }
       });
