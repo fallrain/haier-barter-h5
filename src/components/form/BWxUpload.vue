ad9052eeecb0e1e24e9e4bf658dcdd8097cc3946
王广开 <416266475@qq.com>
2019-12-06 14:56:34 +0800

    每次修改发票改为重置发票url,删除改为url重置为null

--- src/components/form/BWxUpload.vue
+++ src/components/form/BWxUpload.vue
@@ -54,7 +54,7 @@ export default {
       this.$emit('imageuploaded', data, this.imgs);
     },
     delFun(index) {
-      this.$emit('delFun', index, this.imgs);
+      this.$emit('delFun', index);
     },
     imageuploading() {
       Toast.loading('上传中');
@@ -64,7 +64,7 @@ export default {
       wx.ready(() => {
         wx.chooseImage({
           count: 1, // 默认9
-          sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+          sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
           sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
           success: (res) => {
             const localId = res.localIds.join(''); // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
