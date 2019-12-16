03b24e40aa2f82a3d1fcd000ab676c3c46f16a14
mage <mage@sdjictec.com>
2019-12-16 15:11:06 +0800

    添加分享的css

--- src/views/activity/MarketingActivities.vue
+++ src/views/activity/MarketingActivities.vue
@@ -504,4 +504,13 @@ export default {
     width: 493px;
     height: 694px;
   }
+
+  .popContainer {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+  }
 </style>
