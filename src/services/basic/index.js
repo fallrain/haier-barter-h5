32c61af582d7e7db1e58fe71427e8537d57748a2
fhj <haijiao_fan_yt@163.com>
2020-01-08 16:29:43 +0800

    入户服务页面修改

--- src/services/basic/index.js
+++ src/services/basic/index.js
@@ -17,25 +17,48 @@ export default {
     /* 查询产品型号 */
     return axGet(urls.scanQRcode, { url });
   },
-  authorizedUrl(data) {
-    /* 构建微信授权URL给前端 */
-    return axPost(urls.authorizedUrl, data, { noToken: true, });
+  queryCustomerList(data, param) {
+    /* 查询直销员可选择入户服务人员列表 */
+    return axPostJson(urls.queryCustomerList, data, param);
   },
-  sendSms(data) {
-    /* 发送短信验证码给客户 */
-    return axPost(urls.sendSms, data, { noToken: true, });
+  queryAccompanier(param) {
+    /* 搜索随行参与人 */
+    return axGet(urls.queryAccompanier, param);
   },
-  queryIndustry() {
-    /* 查询产业组 */
-    return axGet(urls.queryIndustry);
+  queryAddress(customerId) {
+    /* 查询入户服务人员地址列表 */
+    return axGet(urls.queryAddress(customerId));
   },
-  industryList() {
-    /* 查询全部产业 */
-    return axGet(urls.industryList);
+  createFamilyId(data, param) {
+    /* 创建顾客信息及地址信息 */
+    return axPostJson(urls.createFamilyId, data, param);
   },
-  getBuyIntention() {
-    /* 查询购买意愿 */
-    return axGet(urls.buyIntention);
+  queryFamilyInfo(customerFamilyId) {
+    /* 查询入户服务人员地址列表 */
+    return axGet(urls.queryFamilyInfo(customerFamilyId));
+  },
+  updataFamilyInfo(data, param) {
+    /* 修改顾客的住宅信息、家庭成员 */
+    return axPostJson(urls.updataFamilyInfo, data, param);
+  },
+  deleteHouseHold(customerFamilyAppliancesId) {
+    /* 删除家电信息 */
+    return axGet(urls.deleteHouseHold(customerFamilyAppliancesId));
+  },
+  saveHouseHold(data, param) {
+    /* 保存家电信息 */
+    return axPostJson(urls.saveHouseHold, data, param);
+  },
+  queryProductGroupList() { // 查询产品组信息
+    return axGet(urls.queryProductGroupList);
+  },
+  queryHoseHoldPictures(customerFamilyAppliancesId) {
+    /* 查询家电图片 */
+    return axGet(urls.queryHoseHoldPictures(customerFamilyAppliancesId));
+  },
+  sellerRemind(data, param) {
+    /* 提交提醒 */
+    return axPostJson(urls.sellerRemind, data, param);
   },
   uploadByMediaId(data, param) {
     /* 同步前端微信上传的文件 */
