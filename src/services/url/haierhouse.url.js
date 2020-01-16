47e204afcde4ba16d3c48ec4e44c8c853ee1ad74
fhj <haijiao_fan_yt@163.com>
2020-01-16 17:30:36 +0800

    一站筑家分支合并

--- src/services/url/haierhouse.url.js
+++ src/services/url/haierhouse.url.js
@@ -5,13 +5,13 @@ const urls = {
   addShopInfo: '/deco/shopInfo/add', // 开店申请接口
   addDistrictInfo: '/deco/districtInfo/add', // 小区信息录入接口
   queryShopAndDistrict: '/deco/shopAndDistrict/query', // 店铺申请信息查询接口
-  simpleUpload: '/file/simpleUpload', // 上传图片
+  queryCount: '/activityInfo/user/count', // 获取一站筑家数据分析结果
 
 };
 
 Object.entries(urls).forEach(([key, value]) => {
-  if (value === '/file/simpleUpload') {
-    urls[key] = value;
+  if (value === '/activityInfo/user/count') {
+    urls[key] = `activity${value}`;
   } else {
     urls[key] = baseURL + value;
   }
