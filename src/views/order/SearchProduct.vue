d589d79986613a74bc96eabe5dae20d9ea01f7a6
王广开 <416266475@qq.com>
2020-04-30 18:47:21 +0800

    搜索组件修改； 点击“扫码说明”会显示说明文字和收起按钮；点击“收起”按钮会隐藏说明文字和收起按钮； 2）点击打开摄像头扫描框会进入扫描识别的页面；若扫描识别失败，会显示蒙层和弹窗，提醒“暂无信息，请重新扫描”；点击“取消”按钮隐藏蒙层和弹窗，回到添加产品的页面；点击“继续扫码”按钮会直接跳转到扫码识别的页面；若扫描识别成功，则显示在“已扫产品型号”模块里；再次扫描识别会往已扫产品型号里添加一条数据； 已扫产品型号：当扫描产品型号数量为0时，不显示已扫产品型号模块；当扫描产品型号数量大于0时，才会显示已扫产品型号模块；若型号不对，可点击型号手动进行编辑；点击列表项右侧的X号可删除对应的项并提醒“删除成功”（当只有一条数据时，点击X号删除后数量为0不再显示已扫产品型号模块；当有多条数据时，点击X号删除只会删除对应的产品项）； 4）当已扫产品型号中只有一条数据时，不显示序号；当已扫产品型号为多条数据时，显示序号；

--- src/views/order/SearchProduct.vue
+++ src/views/order/SearchProduct.vue
@@ -1,72 +1,145 @@
 <template>
-  <div>
-    <b-notice-bar
+  <div class="searchProduct">
+    <!--<b-notice-bar
       :show.sync="noticeShow"
       content="温馨提示：点击下方的【扫描】，可扫产品能效贴自动带入产品型号。"
     >
-    </b-notice-bar>
-    <b-search-input
-      v-model="searchVal"
-      @search="search"
-      placeholder="点击搜索型号"
-      v-on:input="inputFunction()"
+    </b-notice-bar>-->
+    <div class="searchProduct-scan">
+      <div class="searchProduct-scan-head">
+        <p class="searchProduct-scan-head-title">方式一：扫码自动获取型号（推荐）</p>
+        <div
+          class="searchProduct-scan-head-tips"
+          @click="toggleDescription"
+        >
+          <span>扫码说明</span>
+          <i
+            class="iconfont icon-jiantou9"
+            :class="[isShowDescription && 'reverse']"
+          ></i>
+        </div>
+      </div>
+      <ol
+        class="searchProduct-scan-description"
+        v-show="isShowDescription"
+      >
+        <li>1.点击下方扫码按钮启动摄像头后，对准顾客所购产品的样机机身上的能效标识二维码（右图红框内）扫描，即可自动获取产品型号。</li>
+        <li>2.扫描完成一个后可点击扫描按钮继续添加商品</li>
+      </ol>
+      <div class="searchProduct-scan-cnt">
+        <div class="searchProduct-scan-icon-par" @click="scanQRCodeAndShow()">
+          <div
+            class="searchProduct-scan-icon-wrap"
+          >
+            <i class="searchProduct-scan-icon iconfont icon-tubiaolunkuo-">
+            </i>
+          </div>
+          <p class="searchProduct-scan-icon-tip">点击打开摄像头</p>
+        </div>
+        <div class="searchProduct-scan-example-img-par">
+          <img
+            src="@/assets/images/order/energy-example@2x.png"
+            class="searchProduct-scan-example-img"
+          >
+          <p class="searchProduct-scan-example-img-tip">机身能效贴示例</p>
+        </div>
+      </div>
+    </div>
+    <div
+      class="searchProduct-scan-product"
+      v-if="scanQRCodeProductList.length"
     >
-      <div class="searchProduct-scan-wrap" @click="scanQRCodePro()">
-        <i class="iconfont icon-saomiao"></i>
-        <span class="searchProduct-scan-inf">扫描</span>
+      <p class="searchProduct-scan-product-title">已扫产品型号：</p>
+      <ul class="searchProduct-scan-product-list">
+        <li
+          class="searchProduct-scan-product-item"
+          v-for="(item,index) in scanQRCodeProductList"
+          :key="item.productCode"
+        >{{index + 1}}.
+          <div class="searchProduct-scan-product-item-cnt">
+            <span class="searchProduct-scan-product-item-name">{{item.productBrandName}}{{item.productGroupName}}</span>
+            <span class="searchProduct-scan-product-item-code">{{item.productModel}}</span>
+          </div>
+          <i
+            class="iconfont icon-chahao searchProduct-scan-product-item-del"
+            @click="delScannedProduct(index)"
+          ></i>
+        </li>
+      </ul>
+      <div class="searchProduct-scan-product-btn-par">
+        <button
+          type="button"
+          class="searchProduct-scan-product-btn"
+          @click="choseProductsAndBack"
+        >完成
+        </button>
       </div>
-    </b-search-input>
-    <ul
-      class="searchProduct-history"
-    >
-      <li
-        class="searchProduct-history-item"
-        v-for="(item,index) in searchListShow"
-        :key="index"
-        @click="onItemClick(item)"
-      >{{item.productModel}}{{item.productBrandName}}
-      </li>
-    </ul>
-    <div class="searchProduct-secret">
-      <div class="searchProduct-secret-title">搜索型号小秘诀</div>
-      <div class="searchProduct-secret-con">
-        尽量输入各型号相似度较小的特有字符串，例如： 若要搜索冰箱BCD-460WDGZ，可以在搜索框输入
-        <strong class="strong">460W</strong>
-        ，而不要输入BCD，因为冰箱都是BCD开头；
+    </div>
+    <div class="searchProduct-scan mt20">
+      <div class="searchProduct-scan-head">
+        <p class="searchProduct-scan-head-title">方式二：手工搜索</p>
       </div>
-      <div class="searchProduct-secret-con">
-        若要搜索空调KFR-72LW/18SNA21AU1，可以在搜索框输入
-        <strong>18SN</strong>
-        ，而不要输入KFR，因为空调都是KFR开头；
+      <b-search-input
+        v-model="searchVal"
+        @search="search"
+        placeholder="点击搜索型号"
+        @input="inputFunction()"
+      >
+        <!--<div class="searchProduct-scan-wrap">
+          <i class="iconfont icon-saomiao"></i>
+          <span class="searchProduct-scan-inf">扫描</span>
+        </div>-->
+      </b-search-input>
+      <ul
+        class="searchProduct-history"
+      >
+        <li
+          class="searchProduct-history-item"
+          v-for="(item,index) in searchListShow"
+          :key="index"
+          @click="onItemClick(item)"
+        >{{item.productModel}}{{item.productBrandName}}
+        </li>
+      </ul>
+      <div class="searchProduct-secret">
+        <div class="searchProduct-secret-title">搜索型号小秘诀</div>
+        <div class="searchProduct-secret-con">
+          尽量输入各型号相似度较小的特有字符串，例如： 若要搜索冰箱BCD-460WDGZ，可以在搜索框输入
+          <strong class="strong">460W</strong>
+          ，而不要输入BCD，因为冰箱都是BCD开头；
+        </div>
+        <div class="searchProduct-secret-con">
+          若要搜索空调KFR-72LW/18SNA21AU1，可以在搜索框输入
+          <strong>18SN</strong>
+          ，而不要输入KFR，因为空调都是KFR开头；
+        </div>
+        <div class="searchProduct-secret-con">
+          若要搜索洗衣机XQG60-QHZB1287，可以在搜索框输入
+          <strong>ZB1287</strong>
+          ，而不要输入XQG，因为洗衣机都是XQG开头；
+        </div>
+        <div class="searchProduct-secret-con">
+          若要搜索热水器JSQ24-Q2(12T)，可以在搜索框输入
+          <strong>24-Q2</strong>
+          ，中间横杠-可以省略。
+        </div>
+        <div class="searchProduct-secret-title  searchProduct-secret-warn mt23">注意：</div>
+        <div class="searchProduct-secret-con">
+          1、大小写都行，
+          <strong>横杠"-" 或斜杠"/" 可以省略</strong>
+          ，但是中文括号"（" 和英文括号"(" 不能输错。
+        </div>
+        <div class="searchProduct-secret-con mb10">2、点击搜索框下面的搜索历史或搜索结果中的型号，可以直接选择完整产品型号。</div>
       </div>
-      <div class="searchProduct-secret-con">
-        若要搜索洗衣机XQG60-QHZB1287，可以在搜索框输入
-        <strong>ZB1287</strong>
-        ，而不要输入XQG，因为洗衣机都是XQG开头；
-      </div>
-      <div class="searchProduct-secret-con">
-        若要搜索热水器JSQ24-Q2(12T)，可以在搜索框输入
-        <strong>24-Q2</strong>
-        ，中间横杠-可以省略。
-      </div>
-      <div class="searchProduct-secret-title  searchProduct-secret-warn mt23">注意：</div>
-      <div class="searchProduct-secret-con">
-        1、大小写都行，
-        <strong>横杠"-" 或斜杠"/" 可以省略</strong>
-        ，但是中文括号"（" 和英文括号"(" 不能输错。
-      </div>
-      <div class="searchProduct-secret-con mb10">2、点击搜索框下面的搜索历史或搜索结果中的型号，可以直接选择完整产品型号。</div>
     </div>
   </div>
 </template>
 
 <script>
 import {
+  Dialog,
   Toast
 } from 'mand-mobile';
-import {
-  BNoticeBar
-} from '@/components/form';
 
 import {
   BSearchInput
@@ -75,7 +148,6 @@ import {
 export default {
   name: 'SearchProduct',
   components: {
-    BNoticeBar,
     BSearchInput,
   },
   created() {
@@ -92,23 +164,58 @@ export default {
   },
   data() {
     return {
-      // 通知栏显示隐藏
-      noticeShow: true,
       // search value
       searchVal: '',
       // 点击的数据
       currentClickItemData: {},
+      // 选中的产品
+      choseProducts: [],
       searchList: [],
       // 搜索历史
       searchHistory: [],
       orderNo: '',
       isProductList: [],
-      recordMode: ''
+      recordMode: '',
+      // 是否显示扫码说明
+      isShowDescription: false,
+      // 扫码之后获取的产品列表
+      scanQRCodeProductList: [
+        {
+          productBrandName: 'ds',
+          productGroupName: 'ds',
+          productModel: 'ds',
+          productCode: 1
+        },
+        {
+          productBrandName: 'ds',
+          productGroupName: 'ds',
+          productModel: 'ds',
+          productCode: 2
+        },
+        {
+          productBrandName: 'ds',
+          productGroupName: 'ds',
+          productModel: 'ds',
+          productCode: 3
+        },
+        {
+          productBrandName: 'ds',
+          productGroupName: 'ds',
+          productModel: 'ds',
+          productCode: 4
+        },
+        {
+          productBrandName: 'ds',
+          productGroupName: 'ds',
+          productModel: 'ds',
+          productCode: 5
+        },
+      ]
     };
   },
   computed: {
     searchListShow() {
-      let arr = [];
+      const arr = [];
       if (this.searchList && this.searchList.length > 0) {
         this.searchList.forEach((item) => {
           const itemStr = `${item.productModel}${item.productBrandName}`;
@@ -116,9 +223,6 @@ export default {
             arr.push(item);
           }
         });
-        if (arr.length === 0) {
-          arr = this.searchList;
-        }
       }
       return arr;
     }
@@ -130,54 +234,123 @@ export default {
         this.searchList = [];
       }
     },
-
-    search() {
-      /* 搜索产品 */
+    toggleDescription() {
+      /* 显示隐藏扫码说明 */
+      this.isShowDescription = !this.isShowDescription;
+    },
+    delScannedProduct(index) {
+      /* 删除已经扫描的 */
+      this.scanQRCodeProductList.splice(index, 1);
+    },
+    search(type = 1) {
+      /**
+         *  搜索产品
+         *  @type 类型 1：搜索并赋值 2：搜索返回值
+         *  */
       if (this.searchVal === '') {
         Toast.failed('请输入搜索内容！');
         return;
       }
       const searchStr = this.searchVal.toUpperCase().replace(/\//g, ' ');
-      this.productService.list(searchStr, '1', '30').then((res) => {
-        // ;
-        if (res.code === 1) {
-          this.searchList = res.data;
-          if (res.data === null) {
-            Toast.failed('暂无信息，请重新搜索');
+      return this.productService.list(searchStr, '1', '30').then(({ code, data }) => {
+        if (code === 1) {
+          if (!data) {
+            Dialog.confirm({
+              content: '暂无信息，请重新搜索',
+              confirmText: '继续扫码',
+              cancelText: '取消',
+              onConfirm: () => {
+                if (type === 1) {
+                  this.scanQRCodeAndShow();
+                } else {
+                  this.scanQRCodePro();
+                }
+              }
+            });
             this.searchVal = '';
+          }
+          if (type === 1) {
+            this.searchList = data || [];
+          } else {
+            return data;
           }
         }
       });
     },
-    scanQRCodePro() {
-      wx.ready(() => {
-        wx.scanQRCode({
-          needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-          // scanType: ['barCode','qrCode'],//qrCode // 可以指定扫二维码还是一维码，默认二者都有
-          success: (res) => {
-            // alert(JSON.stringify(res));
-            const result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-            if (result && typeof result === 'string') {
-              if (result.includes(',')) {
-                this.searchVal = result.split(',')[1];
-                this.search();
-              } else if (result.includes('http')) {
-                this.basicService.scanQRcode(result).then((res2) => {
-                  if (res2.code === 1) {
-                    this.searchVal = res2.data;
-                    this.search();
-                  }
-                });
-              } else {
-                this.searchVal = result;
-                this.search();
-              }
-            }
-          },
-          fail: (res) => {
-            // alert(222);
-            alert(JSON.stringify(res));
+    async scanQRCodePro() {
+      /* 扫码且搜索 */
+      const valid = await this.scanQRCode();
+      if (valid) {
+        this.search();
+      }
+    },
+    async scanQRCodeAndShow() {
+      /* 扫码且展示 */
+      const valid = await this.scanQRCode();
+      if (valid) {
+        const searchList = await this.search(2);
+        if (searchList && searchList.length) {
+          // 扫码只会返回一个产品
+          const product = searchList[0];
+          const {
+            productCode
+          } = product;
+          // 不添加重复产品
+          if (this.scanQRCodeProductList.find(v => v.productCode === productCode)) {
+            return;
           }
+          // 查询价格
+          this.queryPrice({
+            productCode,
+            productGroup: product.productGroup
+          }).then((data) => {
+            // 添加到scanQRCodeProductList
+            this.scanQRCodeProductList.push({
+              ...product,
+              ...data
+            });
+          });
+        }
+      }
+    },
+    choseProductsAndBack() {
+      /* 确认扫码选的产品，并返回 */
+      this.choseProducts = [...this.scanQRCodeProductList];
+      this.$router.back();
+    },
+    scanQRCode() {
+      return new Promise((resolve) => {
+        wx.ready(() => {
+          wx.scanQRCode({
+            needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+            // scanType: ['barCode','qrCode'],//qrCode // 可以指定扫二维码还是一维码，默认二者都有
+            success: (res) => {
+              // alert(JSON.stringify(res));
+              const result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+              if (result && typeof result === 'string') {
+                if (result.includes(',')) {
+                  this.searchVal = result.split(',')[1];
+                  return resolve(true);
+                }
+                if (result.includes('http')) {
+                  this.basicService.scanQRcode(result).then((res2) => {
+                    if (res2.code === 1) {
+                      this.searchVal = res2.data;
+                      return resolve(true);
+                    }
+                  });
+                } else {
+                  this.searchVal = result;
+                  return resolve(true);
+                }
+              }
+            },
+            fail: (res) => {
+              // alert(222);
+              alert(JSON.stringify(res));
+              resolve(false);
+            }
+          });
         });
       });
     },
@@ -195,7 +368,7 @@ export default {
       this.currentClickItemData.productBrandName = item.productBrandName;
       const orderMode = this.recordMode;
       if (orderMode === 'Casarte') {
-        if (item.productBrandName != '卡萨帝') {
+        if (item.productBrandName !== '卡萨帝') {
           Toast.failed('当前是卡萨帝模式，只能选择卡萨帝品牌的产品，请重新选择！');
           return;
         }
@@ -204,26 +377,42 @@ export default {
         this.searchHistory.push(item);
       }
       this.searchVal = '';
-      this.productService.price(item.productCode, item.productGroup).then((res) => {
-        if (res.code === 1) {
-          this.currentClickItemData.price = res.data.price;
-          this.currentClickItemData.industryCode = res.data.industryCode;
-          this.currentClickItemData.industryName = res.data.industryName;
-          this.currentClickItemData.productCode = res.data.productCode;
-          // this.currentClickItemData.productGroup = res.data.productGroup;
-          // this.currentClickItemData.productGroupName = res.data.productGroupName;
-          this.currentClickItemData.productModel = res.data.productModel;
-          this.$router.go(-1);
-        } else {
-          this.currentClickItemData = {};
-        }
+      // 查询价格，并回退上一页
+      this.queryPrice({
+        productCode: item.productCode,
+        productGroup: item.productGroup
+      }).then((data) => {
+        this.choseProducts = [{
+          ...this.currentClickItemData,
+          ...data
+        }];
+        this.$router.go(-1);
       });
     },
-
+    queryPrice({
+      productCode,
+      productGroup
+    }) {
+      /* 查询价格 */
+      return this.productService.price(productCode, productGroup).then(({ code, data }) => {
+        if (code === 1) {
+          return {
+            price: data.price,
+            industryCode: data.industryCode,
+            industryName: data.industryName,
+            productCode: data.productCode,
+            productModel: data.productModel
+          };
+        }
+        this.currentClickItemData = {};
+      });
+    }
   },
   beforeRouteLeave(to, from, next) {
     if (to.name === 'Order.OrderEntry' || to.name === 'Order.OrderModify' || to.name === 'Order.OrderSupplement') {
-      const obj = { product: this.currentClickItemData };
+      const obj = {
+        product: this.choseProducts
+      };
       to.query.temp = JSON.stringify(obj);
       to.params.orderNo = this.orderNo;
       to.params.productList = this.isProductList;
@@ -235,6 +424,10 @@ export default {
 </script>
 
 <style lang="scss">
+  .searchProduct {
+    padding-top: 20px;
+  }
+
   .md-toast-text {
     white-space: normal !important;
   }
@@ -299,5 +492,183 @@ export default {
     margin-left: 12px;
     font-size: 20px;
     writing-mode: vertical-lr;
+  }
+
+  .searchProduct-scan {
+    background: #fff;
+    padding: 30px 24px 40px 24px;
+
+    .bSearchInput-wrap {
+      background: #F5F5F5;
+    }
+
+    .bSearchInput {
+      background: #F5F5F5;
+    }
+  }
+
+  .searchProduct-scan-head {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 48px;
+  }
+
+  .searchProduct-scan-head-title {
+    font-size: 34px;
+    font-weight: 600;
+    color: #333;
+  }
+
+  .searchProduct-scan-head-tips {
+    color: #1969C6;
+    font-size: 28px;
+    line-height: 28px;
+
+    .iconfont {
+      margin-left: 16px;
+
+      &.reverse {
+        display: inline-block;
+        transform: rotateX(180deg);
+        vertical-align: top;
+      }
+    }
+  }
+
+  .searchProduct-scan-cnt {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 40px;
+  }
+
+  .searchProduct-scan-icon-par {
+    text-align: center;
+  }
+
+  .searchProduct-scan-icon-wrap {
+    height: 174px;
+    line-height: 174px;
+    vertical-align: middle;
+  }
+
+  .searchProduct-scan-icon {
+    font-size: 146px;
+    color: #1969C6;
+  }
+
+  .searchProduct-scan-icon-tip {
+    font-size: 24px;
+    color: #4A90E2;
+  }
+
+  .searchProduct-scan-example-img-par {
+    margin-left: 160px;
+    text-align: center;
+  }
+
+  .searchProduct-scan-example-img {
+    width: 108px;
+    height: 174px;
+  }
+
+  .searchProduct-scan-example-img-tip {
+    font-size: 24px;
+    color: #999;
+  }
+
+  .searchProduct-scan-example-img-tip,
+  .searchProduct-scan-icon-tip {
+    margin-top: 27px;
+  }
+
+  .searchProduct-scan-description {
+    border: 1px solid #44BE3C;
+    margin-top: 6px;
+    padding: 18px 9px;
+    color: #333;
+    font-size: 24px;
+    line-height: 40px;
+  }
+
+  .searchProduct-scan-product {
+    margin: 20px;
+    padding-top: 30px;
+    padding-bottom: 30px;
+    background: #fff;
+  }
+
+  .searchProduct-scan-product-title {
+    height: 42px;
+    font-size: 30px;
+    line-height: 42px;
+    color: #333;
+    padding-left: 50px;
+    padding-right: 50px;
+  }
+
+  .searchProduct-scan-product-list {
+    margin-top: 12px;
+    padding-top: 12px;
+    padding-left: 50px;
+    padding-bottom: 17px;
+    max-height: 800px;
+    overflow-y: auto;
+  }
+
+  .searchProduct-scan-product-item {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+    font-size: 24px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  .searchProduct-scan-product-item-cnt {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    width: 540px;
+    min-height: 64px;
+    margin-right: 30px;
+    background: #F5F5F5;
+    padding: 16px 28px;
+    font-size: 24px;
+    word-break: break-all;
+    margin-left: 14px;
+  }
+
+  .searchProduct-scan-product-item-del {
+    color: #999;
+    font-size: 20px;
+  }
+
+  .searchProduct-scan-product-item-name {
+    color: #333;
+    margin-right: 24px;
+  }
+
+  .searchProduct-scan-product-item-code {
+    color: #1969C6;
+  }
+
+  .searchProduct-scan-product-btn-par {
+    width: 100%;
+    text-align: center;
+  }
+
+  .searchProduct-scan-product-btn {
+    width: 300px;
+    height: 60px;
+    background: #fff;
+    color: #1969C6;
+    border: 1px solid #1969C6;
+    border-radius: 10px;
+    font-size: 30px;
+    margin-top: 20px;
   }
 </style>
